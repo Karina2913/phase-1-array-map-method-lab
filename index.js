d620c3379c1a154ef6f75880255f970631f93d7c
Karina Pliego <karinapliegovergara@gmail.com>
2023-06-07 17:50:11 -0500

    Completed Array Map Method Lab

--- index.js
+++ index.js
@@ -12,5 +12,37 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((array) => {
+    const word = array.split(' ');
+    const capitalize = word.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+
+    //combine
+    const combine = capitalize.join(' ');
+    return combine;
+  });
 }
+
+
+/* idea 3
+function titleCased(array) {
+    array.map((word) => {
+    const capitalizeFirst = word.charAt(0).toUpperCase();
+    const lowerCasePart = word.slice(1).toLowerCase();
+    return capitalizeFirst + lowerCasePart;
+  });
+}
+
+titleCased(tutorials);
+*/
+
+/*idea 4
+function titleCased(array) {
+  //code here
+  return array.split(' ').map(function(word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+  });
+
+}
+
+titleCased(tutorials);
+*/
